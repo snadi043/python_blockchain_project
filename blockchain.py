e9321d627d0dfa8ac0a7994813e11371e598d895
snadi043 <snadi043@uottawa.ca>
2026-01-27 18:04:53 -0500

    first python function to add a block to the blockchain environemnt.

---
+++ blockchain.py
@@ -0,0 +1,27 @@
+# This is the initial project setup file to understand the basics of python and make the mind around
+# the blockchain and crypto currency environemnt using python principles.
+
+# This is the basic python data type which is non primitive and is similar to arrays in JavaScript
+# which is called a "List" in python and it's representation is same as in JS which is []
+
+# The term blockchain is the varibale representation in python.
+# There are no keywords like var, int, const in python.
+blockchain = [1]
+
+# This is the syntax for defining a function in python, which is defined with a "def" keyword
+# followed by name of the function and () and :
+# The second line of the function has to be indented to get identified by the python compiler
+# in order to execute the code.
+def add_block():
+    blockchain.append([blockchain[-1], 5.3]) 
+    print(blockchain)
+    # append() -> It is the built in python method for the List data type used to add values to the
+    # list at the end of the existing list.
+    # The values in the list can be accessed with the position which are called "index" which starts
+    # from 0.
+    # blockchain(-1) ->. represents the accessibility to values from the right, usually it is from left.
+    # print() -> it is another python method used to output the values in the terminal when executed.
+
+# To execute the function just call it by the function name along with ().
+add_block()
+add_block()
