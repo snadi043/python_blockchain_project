a838100356d11b598200b1212ec3d03b9ad821f7
snadi043 <snadi043@uottawa.ca>
2026-01-27 18:51:13 -0500

    Implementing various functions to seperate the requirements of the blockchain in the application.

--- blockchain.py
+++ blockchain.py
@@ -8,13 +8,18 @@
 # There are no keywords like var, int, const in python.
 blockchain = [1]
 
+# following a convention that each function should perform single task for implementing code redability
+# and maintainability in the application.
+def get_last_transaction_value():
+    return blockchain[-1]
+
+
 # This is the syntax for defining a function in python, which is defined with a "def" keyword
 # followed by name of the function and () and :
 # The second line of the function has to be indented to get identified by the python compiler
 # in order to execute the code.
-def add_block():
-    blockchain.append([blockchain[-1], 5.3]) 
-    print(blockchain)
+def add_block(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount]) 
     # append() -> It is the built in python method for the List data type used to add values to the
     # list at the end of the existing list.
     # The values in the list can be accessed with the position which are called "index" which starts
@@ -22,6 +27,15 @@ def add_block():
     # blockchain(-1) ->. represents the accessibility to values from the right, usually it is from left.
     # print() -> it is another python method used to output the values in the terminal when executed.
 
+# Function to get user input and use it where ever needed in the code to reduce code repetition.
+def get_user_input():
+    return float(input('Please enter an amount: '))
+
+
+# input() -> It is also an built in python method which accepts the user inputs from the terminal commandline.
+# float() -> It is a data type method to convert the usual user string input to required float format for tx.
+tx_amount = get_user_input()
+
 # To execute the function just call it by the function name along with ().
-add_block()
-add_block()
+add_block(tx_amount, get_last_transaction_value())
+print(blockchain)
