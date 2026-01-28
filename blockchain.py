0e30131a0c2144a5edb09d424aa12e4735c1c7d2
snadi043 <snadi043@uottawa.ca>
2026-01-28 09:30:28 -0500

    Improving the code by splitting the functionality of the project into individual functions and avoiding the repetition of code by implementing loops (for and while) and handling the exit mechanism using the keywords break and while.

--- blockchain.py
+++ blockchain.py
@@ -34,16 +34,55 @@ def add_block(transaction_amount, last_transaction=[1]):
     # blockchain(-1) ->. represents the accessibility to values from the right, usually it is from left.
     # print() -> it is another python method used to output the values in the terminal when executed.
 
-# Function to get user input and use it where ever needed in the code to reduce code repetition.
-def get_user_input():
+    
+# Function to fetch the transaction value
+def get_transaction_value():
     """ Function that is responsible to fetch the user inputs in the float format."""
-    return float(input('Please enter an amount: '))
+    user_input = float(input('Please enter an amount: '))
+    return user_input
+
+# Function to fetch the user input
+def get_user_choice():
+    user_input = input('Choose an option: ')
+    return user_input
+
+# Function to print blockchain elements.
+def print_blockchain_elements():
+    # For loop is the in built python functionality which helps to loop through the iterable data types, here it is "list".
+    # The syntax of for loop is as written below.
+    for block in blockchain:
+        print('Outputting the blocks.')
+        print(block)
+
+tx_amount = get_transaction_value()
+add_block(tx_amount)
 
 
-# input() -> It is also an built in python method which accepts the user inputs from the terminal commandline.
-# float() -> It is a data type method to convert the usual user string input to required float format for tx.
-tx_amount = get_user_input()
+# While loop is another built in python functionality to loop infinetly till a condition is meet.
+# The syntax for the while loop is as follows.
+while True:
+    print('Choose an option from below.')
+    print('1: Add a transaction.')
+    print('2: Output the blocks of the blockchain.')
+    print('q: Quit')
 
-# To execute the function just call it by the function name along with ().
-add_block(tx_amount, get_last_transaction_value())
-print(blockchain)
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        # input() -> It is also an built in python method which accepts the user inputs from the terminal commandline.
+        # float() -> It is a data type method to convert the usual user string input to required float format for tx.
+        tx_amount = get_transaction_value()
+        # To execute the function just call it by the function name along with ().
+        add_block(tx_amount, get_last_transaction_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        # break -> breaks the current execution and quits out of the loop
+        # continue -> continue stops executing the current condition and starts the loop execution  from the first.
+        break
+        # continue
+    else:
+        print('Invalid input. Please select something from the list of choices.')
+    # print('Checking the continue execution.')
+        
+
+print('DONE.')
