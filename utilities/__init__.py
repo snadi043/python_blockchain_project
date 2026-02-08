71fbaf020ca70cbb3a37c3a5a15a4464eabf377b
snadi043 <snadi043@uottawa.ca>
2026-02-08 09:40:33 -0500

    .

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,3 +1,10 @@
+# In python, it is provisioned to provide the controls over what packages can be exported and used within the application.
+# Inorder to do so, we have to configure either the __init__.py file or enable the __all__ variable in the files.
+
+
+# Importing the packages that needs to be handled to be exported in a module or a package.
 from utilities.hash_util import hash_256, hash_block
 
+# Configuring the __all__ variable to tell python that these functions are available to be exported.
+# Other methods from the hash_util file are blocked from accessing and using them elsewhere in the application.
 __all__ = ['hash_256', 'hash_block']
